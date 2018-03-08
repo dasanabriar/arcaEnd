6f123122013e965d123b7e33a900b6f7397c06e0
joca220 <joca220@hotmail.com>
2018-03-08 13:56:15 -0500

    changes
    changes

--- myApp/src/pages/tabs/tabs.ts
+++ myApp/src/pages/tabs/tabs.ts
@@ -4,6 +4,7 @@ import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { SearchPage } from '../search/search';
 import { User } from '../../app/entity/user';
+import { HomePage } from '../home/home';
 @Component({
   templateUrl: 'tabs.html'
 })
@@ -12,6 +13,7 @@ export class TabsPage {
   tab1Root = SearchPage;
   tab2Root = AboutPage;
   tab3Root = ContactPage;
+  tab4Root = HomePage;
 
   constructor(public navParams: NavParams) {
     this.userLogged = navParams.get("userLogged");
